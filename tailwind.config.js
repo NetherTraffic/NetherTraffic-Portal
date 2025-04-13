64990a95786c34f40f4dcf7f51e897f649ad9800
HiTech0926 <2025138508@qq.com>
2025-04-13 12:37:34 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,55 @@
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-      fontFamily: {
-        mojang: ['MojangABC'] // 必须与CSS中的font-family一致
-      },
-        colors: {
-            slate: {
-                900: '#0F172A',
-                800: '#1E293B',
-                700: '#334155',
-              },
-              purple: {
-                400: '#C084FC',
-                500: '#A855F7',
-              },
-              cyan: {
-                400: '#22D3EE',
-                500: '#06B6D4',
-              }
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          50: '#f8fafc',
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cbd5e1',
+          400: '#94a3b8',
+          500: '#64748b',
+          600: '#475569',
+          700: '#334155',
+          800: '#1e293b',
+          900: '#0f172a',
         },
+        cyan: {
+          400: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2',
+        },
+        purple: {
+          400: '#c084fc',
+          500: '#a855f7',
+          600: '#9333ea',
+        }
       },
+      fontFamily: {
+        mojang: ['MojangABC'],
+      },
+      boxShadow: {
+        'neumorphic': '8px 8px 16px #0f172a, -8px -8px 16px #1e293b',
+        'inner-xl': 'inset 0 4px 6px rgba(0, 0, 0, 0.1)'
+      },
+      spacing: {
+        128: '32rem',
+        144: '36rem'
+      },
+      borderWidth: {
+        '3': '3px'
+      },
+      backdropBlur: {
+        xs: '2px',
+      }
     },
-    plugins: [
-      require('@tailwindcss/forms'),
-    ],
-  }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
